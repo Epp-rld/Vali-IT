4d736bebf33e881ee8a751cae66d6373f3128b5c
Epp-rld <epp.aareleid@gmail.com>
2024-11-04 15:30:16 +0200

    Üks selgitav lause siia.

--- src/DayOne.java
+++ src/DayOne.java
@@ -2,7 +2,30 @@
 public class DayOne {
     public static void main(String[] args) {
 
-        System.out.println("Epp");
+      int a = 5;
+      int b = -5;
+
+      // TODO - use && (AND)
+      // TODO - use || (OR)
+
+      // TODO - print out "b is bigger than a"
+      // TODO - print out "a is bigger than b" in else if block
+      // TODO - print out "a and b are equal" in else block
+      //TODO - print out "No specific conditions were met"
+
+
+        //Task 2
+
+        if (b > a && a > 0) {
+            System.out.println("b is bigger than a, and a is positive");
+        } else if  (b < a && b > 0) {
+            System.out.println("a is bigger than b, and b is positive");
+        } else if  (b == a || b < 0 && a < 0) {
+                System.out.println("a and b are either equal or both are negative ");
+        } else {
+            System.out.println("No specific conditions were met");
         }
+
+
     }
 }
