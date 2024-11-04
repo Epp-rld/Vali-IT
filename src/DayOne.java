4aeb9feacab10c894a73ad1dca8db5af9a36d9f9
Epp-rld <epp.aareleid@gmail.com>
2024-11-04 16:10:17 +0200

    If  / Else / Else if conditions

--- src/DayOne.java
+++ src/DayOne.java
@@ -2,26 +2,26 @@
 public class DayOne {
     public static void main(String[] args) {
 
-      int a = 5;
-      int b = -10;
+        int a = 5;
+        int b = -7;
 
-      // TODO - use && (AND)
-      // TODO - use || (OR)
+        //TODO - use && (AND)
+        //TODO - use || (OR)
 
-      // TODO - print out "b is bigger than a"
-      // TODO - print out "a is bigger than b" in else if block
-      // TODO - print out "a and b are equal" in else block
-      //TODO - print out "No specific conditions were met"
+        //TODO - print out "b is bigger than a"
+        //TODO - print out "a is bigger than b" in else if block
+        //TODO - print out "a and b are equal" in else block
+        //TODO - print out "No specific conditions were met"
 
 
         //Task 2
 
         if (b > a && a > 0) {
             System.out.println("b is bigger than a, and a is positive");
-        } else if  (b < a && b > 0) {
+        } else if (b < a && b > 0) {
             System.out.println("a is bigger than b, and b is positive");
-        } else if  (b == a || b < 0 && a < 0) {
-                System.out.println("a and b are either equal or both are negative ");
+        } else if (b == a || b < 0 && a < 0) {
+            System.out.println("a and b are either equal or both are negative ");
         } else {
             System.out.println("No specific conditions were met");
         }
