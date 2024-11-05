ce3870bcb50d45fdea9caee96fec8b5487f15f72
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 12:30:54 +0200

    Scanner / Find largest of three inserted numbers

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 public class DayTwo {
 
     public static void main(String[] args) {
-        double num1;
+
+        //  Write a Java program that takes three numbers from the user and prints the greatest number.
+
+        double num1, num2, num3;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number:");
+        System.out.println("Enter the first number:");
         num1 = sc.nextDouble();
+        System.out.println("Enter the second number:");
+        num2 = sc.nextDouble();
+        System.out.println("Enter the third number:");
+        num3 = sc.nextDouble();
 
-        if (num1 % 2 == 0) {
-            System.out.println("Even number");
-        } else {
-            System.out.println("Odd number");
+        if (num1 >= num2 && num1 >= num2) {
+            System.out.println(num1 + " is the largest number.");
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println(num2 + " is the largest number.");
+        } else if (num3 >= num1 && num3 >= num2) {
+            System.out.println(num3 + " is the largest number.");
         }
-
     }
 }
