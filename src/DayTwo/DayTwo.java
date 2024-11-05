9f79012f249cbb4a564eecff77f80d5d8a8e5719
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 11:00:58 +0200

    Scanner

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -6,7 +6,7 @@ public class DayTwo {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter first number: ");
+    System.out.println("Enter first number:");
         int n = scanner.nextInt();
         System.out.println(n);
 
