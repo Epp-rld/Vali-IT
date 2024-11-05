cde0db49be709d60fd809320772503379d552d41
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 11:48:55 +0200

    Scanner / Simple calculator

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -1,14 +1,25 @@
 package DayTwo;
 
+import java.util.Scanner;
+
 public class DayTwo {
 
     public static void main(String[] args) {
+        double num1, num2;
+        Scanner sc = new Scanner(System.in);
 
-        String hello = "Hello world!";
-        {
-            System.out.println(hello);
+        System.out.println("Enter the first number:");
+        num1 = sc.nextDouble();
+
+        System.out.println("Enter the second number:" );
+        num2 = sc.nextDouble();
+
+        System.out.println("Enter the operator (+,-,*,/):");
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
 
         }
 
     }
-}
