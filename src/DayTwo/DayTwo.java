62dc8612d6524c1b45d11cd514585eb3c2cce0d2
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 16:09:11 +0200

    String / Extract a substring

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -5,8 +5,8 @@ public class DayTwo {
     public static void main(String[] args) {
 
         String string = "Hello, world!";
+        String newString = string.substring(7, 12);
 
-        System.out.println(string.startsWith("Hello"));
-        System.out.println(string.endsWith("world!"));
+        System.out.println(newString);
     }
 }
