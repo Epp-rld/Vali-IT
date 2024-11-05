e8f21043dacb62dfc5458507d976842ed27ebc31
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 11:00:06 +0200

    If  / Else / Else if conditions

---
+++ src/DayTwo/DayTwo.java
@@ -0,0 +1,15 @@
+package DayTwo;
+
+import java.util.Scanner;
+
+public class DayTwo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter first number: ");
+        int n = scanner.nextInt();
+        System.out.println(n);
+
+    }
+
+}
