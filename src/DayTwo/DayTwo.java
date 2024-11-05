0c105c29598df4356dea9ec9d597e0e3f83eef35
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 11:09:14 +0200

    String / Hello world!

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -1,15 +1,14 @@
 package DayTwo;
 
-import java.util.Scanner;
-
 public class DayTwo {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter first number:");
-        int n = scanner.nextInt();
-        System.out.println(n);
+
+        String hello = "Hello world!";
+        {
+            System.out.println(hello);
+
+        }
 
     }
-
 }
