7eac86e2adeb36984f68bdaa5000e6ed4cd006e4
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 15:29:49 +0200

    String / Convert to Uppercase

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -4,10 +4,9 @@ public class DayTwo {
 
     public static void main(String[] args) {
 
-        String university = "University of ";
-        String tallinn = "Tallinn";
-        String fullName = university + tallinn;
+        String hello = "hello world";
+        String helloWorld = hello.toUpperCase();
 
-        System.out.println(fullName);
+        System.out.println(helloWorld);
     }
 }
