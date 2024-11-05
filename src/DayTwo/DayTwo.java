5214439141d92398d008237334fe6fd04e854918
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 15:48:50 +0200

    String / Replace first character

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -5,9 +5,7 @@ public class DayTwo {
     public static void main(String[] args) {
 
         String string = "Hello";
-        int n = string.length();
-        char first = string.charAt(0);
 
-        System.out.println("First character is:" + first);
+        System.out.println(string.replace('H', 'Y'));
     }
 }
