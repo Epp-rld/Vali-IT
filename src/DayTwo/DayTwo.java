9ef2b8e7bbae3ab12f7c44270442a7806aa62da2
Epp-rld <epp.aareleid@gmail.com>
2024-11-05 14:41:22 +0200

    StringBuilder

--- src/DayTwo/DayTwo.java
+++ src/DayTwo/DayTwo.java
@@ -1,29 +1,14 @@
 package DayTwo;
 
-import java.util.Scanner;
-
 public class DayTwo {
 
     public static void main(String[] args) {
 
-        //  Write a Java program that takes three numbers from the user and prints the greatest number.
+        String incomingText = "Aadress Tallinn, kesklinn";
+        StringBuilder sb = new StringBuilder(incomingText);
+        sb.append(", tänav");
 
-        double num1, num2, num3;
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the first number:");
-        num1 = sc.nextDouble();
-        System.out.println("Enter the second number:");
-        num2 = sc.nextDouble();
-        System.out.println("Enter the third number:");
-        num3 = sc.nextDouble();
-
-        if (num1 >= num2 && num1 >= num2) {
-            System.out.println(num1 + " is the largest number.");
-        } else if (num2 >= num1 && num2 >= num3) {
-            System.out.println(num2 + " is the largest number.");
-        } else if (num3 >= num1 && num3 >= num2) {
-            System.out.println(num3 + " is the largest number.");
-        }
+    String string = sb.toString();
+        System.out.println(string);
     }
 }
