4a83b9a298bd52bcda4a48e9d5691a98719f9258
Epp-rld <epp.aareleid@gmail.com>
2024-11-06 15:45:54 +0200

    Methods / return getGreeting

--- src/DayThree/DayThree.java
+++ src/DayThree/DayThree.java
@@ -3,26 +3,11 @@ package DayThree;
 public class DayThree {
 
     public static void main(String[] args) {
-        getArea(4,5);
+    String hello = getGreeting();
+    int length = hello.length();
+        System.out.println(hello);
     }
-// TODO - loo uus meetod private static void getArea(), mis arvutab ristküliku pindala
-// TODO - kutsu see main meetodist välja argumendi väärtustega 4 ja 5
-// TODO - loo uus meetod nimetusega private static void checkArea()
-// TODO - kutsu checkArea() meetod välja getArea() meetodist
-// TODO - prindi konsooli "Area is bigger than 20", kui pindala suurus ületab 20 ...
-//  TODO - ... ja "Area is smaller than 20", kui alla selle
-
-    private static void getArea(int length, int width) {
-        int area = length * width;
-        System.out.println("The area of the rectangle is " + area);
-        checkArea(area);
+    private static String getGreeting(){
+        return "Hello World!";
     }
-
-    private static void checkArea(int area) {
-        if (area > 20) {
-            System.out.println("Area is bigger than 20.");
-        } else {
-            System.out.println("Area is smaller than 20.");
-    }
-}
 }
