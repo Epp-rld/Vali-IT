514f81c14a9f0a4ace152596779c4f479f7b2e9d
Epp-rld <epp.aareleid@gmail.com>
2024-11-06 16:11:47 +0200

    Arrays Arrays.toString

--- src/DayThree/DayThree.java
+++ src/DayThree/DayThree.java
@@ -1,26 +1,11 @@
 package DayThree;
 
+import java.util.Arrays;
+
 public class DayThree {
 
     public static void main(String[] args) {
-        int id = 1;
-        getUser(id);
-    }
-
-    private static void getUser(int id) {
-        getUserFullName(id);
-        getUserAge(id);
-    }
-
-    private static String getUserFullName(int userID) {
-        // Simulate fetching from database
-        System.out.println("Name");
-        String userFullName = "John Doe";
-        return userFullName;
-    }
-    private static int getUserAge(int userID) {
-        System.out.println("Age");
-        int age = 30;
-        return age;
+        int[] m = new int[10];
+        System.out.println(Arrays.toString(m));
     }
 }
