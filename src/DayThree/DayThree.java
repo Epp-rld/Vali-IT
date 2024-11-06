11d23b88be4f5bf9409fb8a5927e51cdde9406cc
Epp-rld <epp.aareleid@gmail.com>
2024-11-06 14:17:19 +0200

    Methods / getPerson, getName, checkUserAge

--- src/DayThree/DayThree.java
+++ src/DayThree/DayThree.java
@@ -3,15 +3,27 @@ package DayThree;
 public class DayThree {
 
     public static void main(String[] args) {
-        getName();
+        getPerson();
+
+        // getAge(); võiks olla ka private static void getName() skoobis.
     }
     // Lisame meetodi, mis prindib ekraanile Hello.
 
-    // TODO - Loo uus meetod getName() ja kutsu main meetodist välja
+    // TODO - Loo uus meetod private static void getName() ja kutsu main meetodist välja
     // TODO - getName() meetodi sees prindi konsooli "John"
+
+    private static void getPerson() {
+        System.out.println("Checking user details:");
+        getName();
+        checkUserAge();
+    }
 
     private static void getName() {
         System.out.println("John");
+        // getAge(); võiks olla ka siin.
+    }
 
+    private static void checkUserAge() {
+        System.out.println("Age is: 30");
     }
 }
