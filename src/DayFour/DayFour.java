219d56b52db994c8a1ea1c09e04ab8a8c650799a
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 11:51:39 +0200

    Loops / Foreach Loop

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -4,12 +4,15 @@ import java.util.Arrays;
 
 public class DayFour {
 
-    // Alternatiivne lahendus - Exercise 3: Modify an Element
-
     public static void main(String[] args) {
-        for (int count = 1; count <= 5; count ++) {
-            System.out.println("Count: " + count);
-            // Prindib kuni 5-ni välja, alates 6-st ei ole tingimus täidetud ja lõpetab.
+        String[] fruits = {"Apple", "Banana", "Orange"};
+        for (String fruit : fruits) {
+            // Siin andsime ette, mida tahame, et ta teeks.
+            // Tahame ühe elemendi kaupa stringi muutujat. Läheme tsüklisse ja prindime välja fruit.
+            // Programm valib array'st ühe elemendi, prindib välja ja liigub uude tsüklisse.
+
+            System.out.println("Fruit: " + fruit);
+            // Tsükliline korduvus. Teev tegevust seni, kuni tervikkogum on läbi käidud.
         }
     }
 }
