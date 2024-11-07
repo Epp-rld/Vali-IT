d7bf1afe2f04bd7a32d0dfa062e939da57a8ee64
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 10:39:23 +0200

    Arrays /  Array Length Property

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -4,17 +4,14 @@ import java.util.Arrays;
 
 public class DayFour {
 
-    // TODO - Exercise 3: Modify an Element:
-    // TODO - Task: Given an array {5, 10, 15, 20}, change the second element to 12
-    //  TODO - ... and print the entire array to see the change.
-    //  TODO - Hint: Assign a new value to array[1], then print the elements individually.
+    // TODO - Exercise 4: Array Length Property:
+    // TODO - Task: Create an array {8, 6, 7, 5, 3, 0, 9} and print its length.
+    //  TODO - Hint: Use array.length to get the length of an array.
 
 
     public static void main(String[] args) {
-        int [] myArray = {5, 10, 15, 20};
-        myArray[1]++;
-        myArray[1]++;
+        int[] myArray = {8, 6, 7, 5, 3, 0, 9};
 
-        System.out.println(myArray[1]);
+        System.out.println(myArray.length);
     }
 }
