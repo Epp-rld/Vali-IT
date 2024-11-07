e055a6ffd14596c0cefd54e819debdd4cb5752db
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 15:42:56 +0200

    Loops / Math.random - Dice

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 public class DayFour {
 
     public static void main(String[] args) {
-        for (int count =1; count <= 5; count ++) {
-            if (count == 3) { // Check if count is 3.
-                continue; // Skip the rest of the loop body for this iteration.
-            }
-            System.out.println("Count is: " + count); // Print count if it's not 3.
+        System.out.println("You rolled a number: " + rollDice());
+    }
+
+    private static int rollDice() {
+            return (int) (Math.random() * 6) + 1;
         }
     }
-}
