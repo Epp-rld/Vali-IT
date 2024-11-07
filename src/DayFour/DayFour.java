627aade8aeb61d219728a6e9a6235388da0c14bf
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 15:26:00 +0200

    Loops / Continue

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -6,10 +6,9 @@ public class DayFour {
 
     public static void main(String[] args) {
         for (int count =1; count <= 5; count ++) {
-            if (count ==3) { // Check if count is 3.
-                break; // Exit the loop if count is 3.
+            if (count == 3) { // Check if count is 3.
+                continue; // Skip the rest of the loop body for this iteration.
             }
-            System.out.println("Well done, we found Markus on the list.");
             System.out.println("Count is: " + count); // Print count if it's not 3.
         }
     }
