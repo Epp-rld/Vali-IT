0c72b29db5b0e06613e4f6384b5898159cc1ad99
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 14:51:58 +0200

    Loops / While Loop Exercise 3: Sum of Numbers - main meetodist väljakutsumine

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 
 public class DayFour {
 
-    // TODO - Exercise 5: Multiplication Table
-    // TODO - Write a program that uses a for loop to print the multiplication table of 7.
-    // TODO - Provide the table up to 7 x 10 = 70, meaning limit loop to repeat 10 times.
-    // TODO - Hint: Wjen printing the results, use variables values and Strings.
+    // TODO - Exercise 3: Sum of Numbers - välja kutsuda main meetodist
 
     public static void main(String[] args) {
-        int n = 7;
-        for (int i = 1; i <= 10; i++) {
-
-                System.out.println(n + " x " + i + " = " + n * i);
+       sumOfNumbers();
+    }
+        private static void sumOfNumbers() {
+           int sum = 0;
+           int count = 1;
+           while (count <= 100) {
+               sum += count;
+               count++;
+           }
+            System.out.println(sum);
             }
         }
-    }
