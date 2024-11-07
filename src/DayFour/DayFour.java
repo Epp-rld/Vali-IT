33c7e159214318a0585d82dd9cceddedffa9f885
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 16:21:48 +0200

    Loops / For Loop / Exercise 1: Find the first even number in a list

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -1,14 +1,23 @@
 package DayFour;
 
-import java.util.Arrays;
-
 public class DayFour {
 
+    // TODO - Exercise 1: Find the first even number in a list
+    // TODO - Given an array of numbers, use a for loop to find the first even number in the array
+    // TODO - Once you find it, print it and exit the loop.
+
     public static void main(String[] args) {
-        System.out.println("You rolled a number: " + rollDice());
+        firstEvenNumber();
+
     }
 
-    private static int rollDice() {
-            return (int) (Math.random() * 6) + 1;
+    private static void firstEvenNumber() {
+        int[] myArray = {1, 3, 7, 4, 9, 10};
+        for (int i = 1; i < myArray.length; i++) {
+            if (myArray[i] % 2 == 0) {
+                System.out.println("The first even number is: " + myArray[i]);
+                break;
+            }
         }
     }
+}
