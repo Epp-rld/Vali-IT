0a0c66f6257ce3d6f6d90476b7353edfe6f96232
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 10:18:20 +0200

    Arrays / Declare and initialize an array

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -1,17 +1,21 @@
 package DayFour;
 
+import java.util.Arrays;
+
 public class DayFour {
 
+    // TODO - Exercise 1: Declare and initialize an array:
+    // TODO - Declare an integer array of size 5 and initialize it with values: {1, 2, 3, 4, 5}.
+    // TODO - Hint: Use int[] myArray = {1, 2, 3, 4, 5}.
+
     public static void main(String[] args) {
-        int [] firstArray = new int[5];
-        // {0, 0, 0, 0, 0}
-        firstArray[3]++;
-        // Neljandat elementi (seda tähistab [3]) kasvatame ++-ga ühe võrra (ehk 0 --> 1):
-        // {0, 0, 0, 1, 0}
-        System.out.println(firstArray[3]);
-        // Süsteem prindib välja neljanda elemendi väärtuse 1.
+        int [] myArray = {0, 1, 2, 3, 4};
+        myArray[0]++;
+        myArray[1]++;
+        myArray[2]++;
+        myArray[3]++;
+        myArray[4]++;
 
-
-
+        System.out.println(Arrays.toString(myArray));
     }
 }
