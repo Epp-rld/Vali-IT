c0099a5b36445eac4d718dcfa637b485400dcfa7
Epp-rld <epp.aareleid@gmail.com>
2024-11-07 11:31:32 +0200

    Loops / While Loop

--- src/DayFour/DayFour.java
+++ src/DayFour/DayFour.java
@@ -4,15 +4,18 @@ import java.util.Arrays;
 
 public class DayFour {
 
-    // TODO - Exercise 5: Sum of Elements (Without Loop):
-    // TODO - Task: Calculate the sum of elements in an array {4, 2, 8, 1, 6} and store it in a variable.
-    //  TODO - Hint: Manually add each element: int sum = array[0] + array[1] + ... + array[4];
-
+    // Alternatiivne lahendus - Exercise 3: Modify an Element
 
     public static void main(String[] args) {
-        int[] myArray = {4, 2, 8, 1, 6};
-        int sum = myArray[0] + myArray[1] + myArray[2] + myArray[3] + myArray[4];
+        int count = 1; // Loome int-tüüpi muutuja algväärtusega 1.
+        while (count <= 5) { // Alati väiksem või võrdne kui 5.
+            // Määrame, kui kaua tahame, et koodiplokk käiks.
+            System.out.println("Count is: " + count);
+            count++; // Alustab 1-st ja uuel ringil prindib konsooli 2, järgmisel real 3 jne.
+            // Tsükkel lõpetatakse 5-ga.
+            System.out.println("Count is after count++: " + count);
+            //Prindib välja ka muutuja väärtuse igal hetkel.
+        }
 
-        System.out.println(sum);
     }
 }
