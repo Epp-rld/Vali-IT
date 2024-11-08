c43a37c7691bd97901cc524e3c0a7e824b068546
Epp-rld <epp.aareleid@gmail.com>
2024-11-08 10:20:35 +0200

    Loops / For Loop / Exercise 5: Find the first number greater than 50 in an array

--- src/DayFive/DayFive.java
+++ src/DayFive/DayFive.java
@@ -2,28 +2,24 @@ package DayFive;
 
 public class DayFive {
 
-    // TODO - Print the Sum of Non-Negative Numbers Until a Negative NUmber is Found
-    // TODO - Given an array of numbers, use a for loop to calculate
-    //  the sum of all non-negative numbers.
-    // TODO - Stop the loop when a negative number is encountered.
-    // TODO - Hint: int[] numbers {5, 12, 3, 7, -4, 8, 2}
-
+    // TODO -/  Exercise 5: Find the first number greater than 50 in an array
+    // TODO - Given an array of numbers, use a loop to find the first number greater than 50.
+    // TODO - Print it and exit the loop immediately after.
+    // TODO - int[] numbers = {23, 45, 39, 51, 48, 60};
 
     public static void main(String[] args) {
-        SumOfNonNegativeNumbers();
+        firstGreaterThanFifty();
 
     }
 
-    private static void SumOfNonNegativeNumbers() {
-        int[] numbers = {5, 12, 3, 7, -4, 8, 2};
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {// Check for non-neg and accumulative sum.
-            if (numbers[i] >= 0) {
-                sum += numbers[i];
-            } else {
-                break; // Exit the loop when a neg number is found.
+    private static void firstGreaterThanFifty() {
+        int[] numbers = {23, 45, 39, 51, 48, 60};
+
+        for (int num : numbers) {
+            if (num > 50) {
+                System.out.println("The first number over 50 is: " + num);
+                break;
             }
         }
-        System.out.println("The sum is: " + sum);
     }
 }
