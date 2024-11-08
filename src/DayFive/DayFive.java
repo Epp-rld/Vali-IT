f48c0af48f680918e6b693aec590daca29166936
Epp-rld <epp.aareleid@gmail.com>
2024-11-08 15:56:22 +0200

    Loops / ArrayList / Exercise 1: Fix Broken code

--- src/DayFive/DayFive.java
+++ src/DayFive/DayFive.java
@@ -4,27 +4,14 @@ import java.util.List;
 
 public class DayFive {
     public static void main(String[] args) {
-        List<String> listOfNames = new ArrayList<>();
-        listOfNames.add("Alex");
-        listOfNames.add("Bob");
-        listOfNames.add("Charlie");
+        ArrayList<Integer> listOfNumbers = new ArrayList<>();
 
-        System.out.println(listOfNames);
+        listOfNumbers.add(10);
+        listOfNumbers.add(20);
+        listOfNumbers.add(30);
 
-        for (String name : listOfNames) {
-            System.out.println(name);
+        for (int i = 0; i < listOfNumbers.size(); i++) {
+            System.out.println("Element at index " + i + ": " + listOfNumbers.get(i));
         }
-
-        listOfNames.remove(1); // Eemaldame ühe elemendi ehk antud juhul Bobi.
-
-        System.out.println(" ");
-
-        System.out.println("Print out name: " + listOfNames.get(0));
-        System.out.println("Print out name: " + listOfNames.get(1));
-
-        listOfNames.set(1, "John"); // Asendame nime 1. positsioonil ehk Bobi Johniga.
-
-        System.out.println("Names after modification: " + listOfNames);
-
     }
 }
