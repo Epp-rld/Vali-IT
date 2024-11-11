9b68f15c328b63d3185369d0374310e4e87381fd
Epp-rld <epp.aareleid@gmail.com>
2024-11-11 10:39:48 +0200

    Set, HashSet - Exercise 1: Find unique words in a sentence

--- src/DayFive/DayFive.java
+++ src/DayFive/DayFive.java
@@ -2,16 +2,24 @@ package DayFive;
 import java.util.ArrayList;
 import java.util.List;
 
+// Exercise 2 - Removing Elements from a List in a Loop
+//The code below attempts to remove all even numbers from a list of Integers. However, it throws an IndexOutOfBoundsException.
+
 public class DayFive {
     public static void main(String[] args) {
-        ArrayList<Integer> listOfNumbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
 
-        listOfNumbers.add(10);
-        listOfNumbers.add(20);
-        listOfNumbers.add(30);
-
-        for (int i = 0; i < listOfNumbers.size(); i++) {
-            System.out.println("Element at index " + i + ": " + listOfNumbers.get(i));
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
         }
+
+        for (int i = numbers.size()-1; i >= 0; i--) { // Alustame lugemist 10st allapoole. Lisasime -1,
+            // seega ei alusta nüüd enam 10. kohalt, vaid seame alguseks -10.
+            if (numbers.get(i) % 2 == 0) {
+                numbers.remove(i);
+            }
+        }
+
+        System.out.println("List after removing even numbers: " + numbers);
     }
-}
+    }
---
+++ src/DaySix/DaySix.java
@@ -0,0 +1,30 @@
+package DaySix;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+
+public class DaySix {
+    public static void main(String[] args) {
+        // Exercise 1: Find unique words from a sentence
+        //Write a program that takes a sentence as input, splits it into words,
+        // and then stores each unique word in a set.
+        // Finally, print the set of unique words.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentence here: ");
+        String sentence = sc.nextLine(); // Kursor liigub rida allapoole.
+        sentence = sentence.replace('.',' '); // Tunneb ära et, punktiga lõppev sõna on sama sõna, mis lause sees olev.
+        String[] words = sentence.split("\\s+"); // Arvestab tühikuid, tab'e, white space'e.
+        // This will return an array of words.
+        Set<String> uniqueWords = new HashSet<>(); // Use a HashSet to store unique words.
+        // Sets automatically remove duplicate words.
+
+        // Loop through the words array and add each word to the uniqueWords set.
+        // The set will ignore duplicates.
+        for (String word : words) {
+            uniqueWords.add(word.toLowerCase()); // Et programm kohtleks suur- ja väiketähega algavat sama sõna võrdselt.
+        }
+        System.out.println("Unique words in the sentence: " + uniqueWords);
+        sc.close();
+    }
+}
+
